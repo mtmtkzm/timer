c887c4ea75d3bafaabf846b9a9670c586e0dba2f
mtmtkzm <hello@mtmtkzm.com>
2017-08-30 08:38:34 +0900

    Update Picker選択できるように

--- app/App.js
+++ app/App.js
@@ -13,6 +13,8 @@ export default class CountDownTimer extends Component {
 
     this.state = {
       timer: ['03', '00'], // [hours, min]
+      minutes: 3,
+      seconds: 0,
       containerStyles: styles.containerDefault
     };
 
@@ -72,23 +74,27 @@ export default class CountDownTimer extends Component {
       <View style={[ styles.container ]}>
         <View style={[ styles.picker ]}>
           <View style={[ styles.pickerMinutes ]}>
-            <Picker itemStyle={[ styles.pickerItem ]} selectedValue="3">
+            <Picker itemStyle={[ styles.pickerItem ]}
+                    selectedValue={this.state.minutes}
+                    onValueChange={(itemValue, itemIndex) => this.setState({minutes: itemValue})}>
               { n.map(i => ( <Picker.Item label={i.toString()} key={i} value={i} /> )) }
             </Picker>
             <Text style={[ styles.pickerMinutesUnit ]}>分</Text>
           </View>
 
           <View style={[ styles.pickerSeconds ]}>
-            <Picker itemStyle={[ styles.pickerItem ]} selectedValue="0">
+            <Picker itemStyle={[ styles.pickerItem ]}
+                    selectedValue={this.state.seconds}
+                    onValueChange={(itemValue, itemIndex) => this.setState({seconds: itemValue})}>
               { n.map(i => ( <Picker.Item label={i.toString()} key={i} value={i} /> )) }
             </Picker>
             <Text style={[ styles.pickerSecondsUnit ]}>秒</Text>
           </View>
         </View>
         <View style={[ styles.timer ]}>
-          <Text style={[ styles.timerMinutes]}>{this.state.timer[0]}</Text>
+          <Text style={[ styles.timerMinutes]}>{this.state.minutes}</Text>
           <Text style={[ styles.timerColon ]}>:</Text>
-          <Text style={[ styles.timerSeconds]}>{this.state.timer[1]}</Text>
+          <Text style={[ styles.timerSeconds]}>{this.state.seconds}</Text>
         </View>
       </View>
     );
